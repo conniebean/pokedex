8ef3d9de9660133b4a530f405b897dd50467f678
conniebean <77754785+conniebean@users.noreply.github.com>
2022-11-28 16:56:38 -0500

    started some routes

---
+++ tests/Http/Controllers/PokemonControllerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Http\Controllers;
+
+use App\Http\Controllers\PokemonController;
+use Illuminate\Http\Client\Request;
+use Illuminate\Support\Facades\Auth;
+use Tests\TestCase;
+
+class PokemonControllerTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function test_it_can_hit_a_route()
+    {
+
+    }
+}
